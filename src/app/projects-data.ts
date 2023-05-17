e37f09a36f9b78c9f2e68637391b6ffe30ada4d8
allychoung <allychoung@gmail.com>
2023-05-17 10:51:20 -0400

    new working directory

--- src/app/projects-data.ts
+++ src/app/projects-data.ts
@@ -5,31 +5,57 @@ const projects = [
       description: "minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, ",
       tasks: ["task 1", "task 2"], 
       dateRange: "January - April 2023",
-      github: "",
+      links: [
+        {
+          faIconClass: "", 
+          url: ""
+        },
+      ],
      },
     { 
       src: "../assets/HomeNotes_512x512.png", 
       projectName: "HomeNotes Web Application", 
       description: "minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, ",
       tasks: [], 
-      dateRange: "June - August 2022",
-      github: "",
+      dateRange: "July - August 2022",
+      links: [
+        {
+          faIconClass: "", 
+          url: ""
+        },
+      ],
     },
     { 
       src: "../assets/Sunflower_sky_backdrop.jpg", 
       projectName: "Movie Reviews Web Application", 
       description: "minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, ",
-      tasks: ["task 1"], 
-      dateRange: "November - December 2022",
-      github: "",
+      tasks: [
+        "Designed a full-stack web application using React, Node, Express and MongoDB that allows users to register accounts and search and review movies",
+        "Performed queries to a third-party API, The Movie Database, designed and implemented several schemas as MongoDB collections, and linked them to the web application through Mongoose",
+        "Provided support for user login sessions through Express.js cookies and supported multiple user roles",
+      ], 
+      dateRange: "December 2021",
+      links: [
+        {
+          faIconClass: "", 
+          url: ""
+        },
+      ],
     },
     { 
       src: "../assets/Sunflower_sky_backdrop.jpg", 
       projectName: "Image Processing Application", 
       description: "minim veniam, quis nostrud consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, ",
-      tasks: [], 
-      dateRange: "June - July 2022",
-      github: "",
+      tasks: [
+        "Designed a Java MVC program that can filter an image from a set of image processing algorithms",
+        "Created a GUI application using Swing that renders provided images and allows users to upload and select filters to apply to JPG, PNG, and PPM image files"], 
+      dateRange: "July 2022",
+      links: [
+        {
+          faIconClass: "", 
+          url: "https://gitfront.io/r/achouachou/4EpME3gF9mHi/ood-image-processing/"
+        },
+      ],
     },
   ];
   
