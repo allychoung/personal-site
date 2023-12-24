2a78343ff8ca86a6e8b90f70a22b5714a9fe6278
allychoung <allychoung@gmail.com>
2023-12-24 15:26:12 -0500

    add angular material dependency

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeScreenComponent } from './home-screen/home-screen.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatGridListModule, MatGridList } from '@angular/material/grid-list'
 
 @NgModule({
   declarations: [
@@ -12,7 +14,9 @@ import { HomeScreenComponent } from './home-screen/home-screen.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatGridListModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
