c55a58bcf5e7b2f09a34a93e3122cddfe50c487a
allychoung <allychoung@gmail.com>
2024-10-12 15:24:24 -0400

    fix up styling, skills section

--- src/data/skills-data.ts
+++ src/data/skills-data.ts
@@ -5,7 +5,7 @@ const skills = {
       ], 
   pl: [
     {faIcon: "fa-brands fa-java", name: "Java", ranking: 4},
-    {faIcon: "fa-brands fa-square-js", name: "JavaScript", ranking: 4},
+    {faIcon: "fa-brands fa-square-js", name: "JavaScript/TypeScript", ranking: 4},
     {faIcon: "fa-brands fa-html5", name: "HTML/CSS", ranking: 5},
     {icon: null, name: "Racket", ranking: 4},
     {icon: null, name: "SQL", ranking: 3},
@@ -18,7 +18,11 @@ const skills = {
     {icon: null, name: "MongoDB"},
     {icon: null, name: "Git"},
     {icon: null, name: "MySQL"},
-
+    {icon: null, name: "Angular"},
+    {icon: null, name: "Ruby"},
+    {icon: null, name: "D3.js"},
+    {icon: null, name: "Rails"},
+    {icon: null, name: "Spring Boot"},
   ], 
 };
 
